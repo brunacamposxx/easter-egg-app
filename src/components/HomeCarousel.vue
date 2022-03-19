4e899c4072dc95c593344163a9a3d661e4e5453f
Vinicius Machado Ribeiro <viniciusmachadoribeiro@hotmail.com>
2022-03-19 12:12:09 -0300

    carousel adicionado na HomeView

---
+++ src/components/HomeCarousel.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home-carousel">
+    <v-carousel bordered cycle height="30vh">
+      <v-carousel-item v-for="ovo of algunsOvos" :key="ovo.id">
+        <v-img :src="ovo.imagem" :alt="ovo.nome"/>
+      </v-carousel-item>
+    </v-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeCarousel',
+  props: {
+    ovos: {
+      type: Array,
+      required: true
+    }
+  },
+  computed: {
+    algunsOvos() {
+      // deep copy
+      const algunsOvos = [ ...this.ovos ]
+
+      return algunsOvos.filter(ovo => [6, 7, 10].includes(ovo.id))
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,30 @@
 <template>
-  <h1>Estou na rota de Home</h1>
+  <v-container>
+    <HomeCarousel :ovos="ovos"/>
+  </v-container>
 </template>
 
 <script>
+  import HomeCarousel from '../components/HomeCarousel.vue'
 
   export default {
     name: 'HomeView',
-
-    // components: {
-    //   HomeView,
-    // },
+    components: {
+      HomeCarousel,
+    },
+    data() {
+      return {
+        ovos: []
+      }
+    },
+    created() {
+      fetch('https://it3-hbn-default-rtdb.firebaseio.com/ovosPascoa.json')
+      .then(res => res.json())
+      .then(json => { this.ovos = json })
+    }
   }
 </script>
+
 <style scope>
+
 </style>
