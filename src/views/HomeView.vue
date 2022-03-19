c380f58995f8bce9beca64d26eda816f625d03af
RebecaCamilo <rebecacamilo3@gmail.com>
2022-03-19 17:14:19 -0300

    Quebra de linha na pag inicio

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,8 +3,10 @@
     <HomeCarousel :produtos="produtos"/>
     <v-card color="brown darken-4 " dark>
       <v-card-title class="mt-5">A páscoa é uma época muito especial para a maioria dos brasileiros e a tradição de presentear amigos e familiares com chocolates ganha um sabor diferente com a oferta de ovos de Páscoa caseiros.
+        <br>
 
 O fechamento de lojas e shoppings em diversas cidades do país, por causa da pandemia, pode favorecer o pequeno comerciante, que buscou na venda de chocolates uma fonte de renda extra para diminuir os impactos da crise econômica.
+<br>
 
 Navegue por nosso app e conheça nossos produtos caseiros para deliciar você a a todos seus entes queridos.</v-card-title>
     </v-card>
